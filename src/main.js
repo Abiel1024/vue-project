b224d99461b13187c724989b3baba16a77eacc2d
Abiel <15957198609@163.com>
2018-04-27 11:56:47 +0800

    import plugin iview

--- src/main.js
+++ src/main.js
@@ -4,7 +4,38 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import './main.less'
+import {
+  Button,
+  Icon,
+  Input,
+  Checkbox,
+  Switch,
+  Table,
+  Select,
+  Option,
+  DatePicker,
+  Cascader,
+  InputNumber,
+  Message,
+  Modal
+} from 'iview'
 
+Vue.component('Button', Button)
+Vue.component('Icon', Icon)
+Vue.component('i-Input', Input)
+Vue.component('Input', Input)
+Vue.component('Checkbox', Checkbox)
+Vue.component('i-Switch', Switch)
+Vue.component('Table', Table)
+Vue.component('Select', Select)
+Vue.component('Option', Option)
+Vue.component('DatePicker', DatePicker)
+Vue.component('Cascader', Cascader)
+Vue.component('InputNumber', InputNumber)
+Vue.component('Modal', Modal)
+Message.config({top: 24, duration: 3})
+Vue.prototype.$Message = Message
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
