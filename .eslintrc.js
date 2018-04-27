54e950b3933abcdad5c0da9735a79555b3b1f844
Abiel <15957198609@163.com>
2018-04-27 11:29:52 +0800

    eslintrc remove indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     'space-before-function-paren': 'off',
+    'indent': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
