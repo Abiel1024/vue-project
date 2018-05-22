b14ca2826e44af83547cf725f0e7725b3c56e577
Abiel <abiel.zhang@gmail.com>
2018-05-22 18:32:33 +0800

    fix

--- src/util/request.js
+++ src/util/request.js
@@ -36,10 +36,11 @@ const checkStatus = response => {
 function checkCode(res) {
   // 如果code异常(这里已经包括网络错误，服务器错误，后端抛出的错误)，可以弹出一个错误提示，告诉用户
   if (res.status === -404) {
-    alert(res.msg)
+    // console.log(res.msg)
   }
   if (res.data && (!res.data.success)) {
-    alert(res.data.msg)
+    // console.log(res)
+    // console.log(res.data.msg)
   }
   return res
 }
@@ -69,7 +70,7 @@ export default {
   get(url, params) {
     return axios({
       method: 'get',
-      baseURL: 'http://you.host.com',
+      baseURL: '',
       url,
       params, // get 请求时带的参数
       timeout: 10000,
