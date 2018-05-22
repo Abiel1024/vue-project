6b94d9c6689c42027d18869743a8f13d3131f907
Abiel <abiel.zhang@gmail.com>
2018-05-22 18:31:45 +0800

    proxyTabel

--- config/index.js
+++ config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': { // 会将把api的接口代理到要target去
+        target: 'http://127.0.0.1:8888', // 要访问的ip
+        changeOrigin: true,
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
